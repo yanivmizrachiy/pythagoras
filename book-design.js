de289c2a8d9bd18c0ca015e306b549ca1816daa5
Yaniv Raz <yanivmiz77@gmail.com>
2026-08-30 19:47:30 +0300

    chore(cache): bump compact UI stylesheet version

--- book-design.js
+++ book-design.js
@@ -2,7 +2,7 @@
 if(!document.querySelector('link[data-pythagoras-ui-controls]')){
   const l=document.createElement('link');
   l.rel='stylesheet';
-  l.href='ui-controls.css?v=2';
+  l.href='ui-controls.css?v=3';
   l.dataset.pythagorasUiControls='1';
   document.head.append(l);
 }
